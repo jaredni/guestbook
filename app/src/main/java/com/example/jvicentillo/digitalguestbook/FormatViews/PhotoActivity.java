4df2d495559f1976a6b82076a559e959c44b0343
Jared Vicentillo <jared.vicentillo@gmail.com>
2019-11-13 14:51:26 +0800

    Initial commit for image save

--- app/src/main/java/com/example/jvicentillo/digitalguestbook/FormatViews/PhotoActivity.java
+++ app/src/main/java/com/example/jvicentillo/digitalguestbook/FormatViews/PhotoActivity.java
@@ -4,9 +4,13 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +20,14 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.jvicentillo.digitalguestbook.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PhotoActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -34,6 +46,10 @@ public class PhotoActivity extends AppCompatActivity {
         if (requestCode == 1) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getPhoto();
+            }
+        } else if (requestCode == 2) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveImage();
             }
         }
     }
@@ -65,5 +81,48 @@ public class PhotoActivity extends AppCompatActivity {
     public void clearDrawing(View view) {
         DrawableImageView div = findViewById(R.id.background);
         div.removeDrawing();
+    }
+
+    public Bitmap viewToBitmap(View view) {
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
+    }
+
+    public void saveImage() {
+        createDirectory();
+        /*Bitmap bitmap = viewToBitmap(view);
+        try {
+            FileOutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory() + "/GuestBook/Test/Images/first.png");
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, output);
+            output.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    public void createDirectory() {
+        File folder = new File(getExternalCacheDir(), "NewDirectory");
+
+        boolean success = true;
+        if (!folder.exists()) {
+            success = folder.mkdirs();
+        }
+        if (success) {
+            Log.i("status", "success");
+        } else {
+            Log.i("status", Environment.getExternalStorageDirectory().toString());
+        }
+    }
+
+    public void requestSaveImage(View view) {
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+        } else {
+            saveImage();
+        }
     }
 }
