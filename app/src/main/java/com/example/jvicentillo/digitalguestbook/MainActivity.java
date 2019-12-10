b4a9a3e221c45016ccdfdc3c1847a76d8fd31d50
Jared Vicentillo <jared.vicentillo@gmail.com>
2019-12-11 05:31:50 +0800

    Remove unused ffmpeg

--- app/src/main/java/com/example/jvicentillo/digitalguestbook/MainActivity.java
+++ app/src/main/java/com/example/jvicentillo/digitalguestbook/MainActivity.java
@@ -4,10 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
-
 public class MainActivity extends AppCompatActivity {
-    private FFmpeg ffmpeg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
